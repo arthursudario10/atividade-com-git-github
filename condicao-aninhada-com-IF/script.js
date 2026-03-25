7c199ff9a018d50553d38e02f9ccaaf613660947
I1HSD <arthursudario.10@gmail.com>
2026-03-25 10:43:36 -0300

    Condicional utilizando o operador aninhado

---
+++ condicao-aninhada-com-IF/script.js
@@ -0,0 +1,14 @@
+let renda = Number(prompt("Digite sua renda:"));
+
+if(!isNaN(renda)){
+    if (renda >= 3000) {
+        let nomeLimpo = prompt("Nome limpo? (sim ou nao)");
+        if (nomeLimpo === "sim") {
+            alert("Financiamento aprovado");
+        } else {
+            alert("Nome negativado");
+        }
+    } else {
+        alert("Renda insuficiente");
+    }
+}
