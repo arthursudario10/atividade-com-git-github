e886f4321a61ce9e396126d969a7b5d192c0407d
I1HSD <arthursudario.10@gmail.com>
2026-03-25 08:23:29 -0300

    Condicional utilizando o operador OR/OU

---
+++ condicao-complexa-OR/script.js
@@ -0,0 +1,9 @@
+let idade = Number("Digite sua idade:");
+let estudante = prompt("É estudante? (sim ou nao)");
+let professor = prompt("É professor? (sim ou nao)");
+
+if (idade >=60 || estudante === "sim" || professor === "sim") {
+    alert("Tem direito ao desconto");
+} else {
+    alert("Não tem desconto");
+}
